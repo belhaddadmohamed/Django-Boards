536b8383ead992dca73823d1094febea6a3f2846
belhaddadMohamed <belhaddad.mohamed7@gmail.com>
2024-02-27 23:34:28 +0100

    Update database to 'sqlite' instead of 'postgres'

--- boards/admin.py
+++ boards/admin.py
@@ -4,17 +4,19 @@ from boards.models import Board, Post, Topic
 from django.contrib.auth.models import Group
 from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
-admin.site.register(Post)
 
-# unregister GROUP
-admin.site.unregister(Group)
 
-# customize header and title
-admin.site.site_header = "Boards"
-admin.site.site_title = "Boards Admin Panel"
+# customize the Board model 
+class InlineTopic(admin.StackedInline):
+    model = Topic  
+    extra = 2  # show 2 samples
 
-# customize the Topic model ============================================================
+class BoardAdmin(admin.ModelAdmin):
+    # inlines should have Foriegn key with the other model
+    inlines = [InlineTopic]  
+
+
+# customize the Topic model 
 class TopicAdmin(admin.ModelAdmin):
     fields = ('subject', 'created_by',)
     list_display = ('subject', 'board', 'created_by', 'combine_subject_and_board',)
@@ -26,20 +28,17 @@ class TopicAdmin(admin.ModelAdmin):
     def combine_subject_and_board(self, obj):
         return " {}__{} " . format(obj.subject , obj.board)
 
-# admin.site.register(Topic, TopicAdmin)
 
-# customize the Board model =============================================================
-class InlineTopic(admin.StackedInline):
-    model = Topic  
-    extra = 2  # show 2 samples
-
-class BoardAdmin(admin.ModelAdmin):
-    inlines = [InlineTopic]  # inlines should have Foriegn key with the other model
-
-admin.site.register(Board, BoardAdmin)
-
-
-# Import / Export database as a file csv ... =============================================
+# Import / Export database (csv, excel...)
 @admin.register(Topic)
 class TopicAdmin(ImportExportModelAdmin):
     pass
+
+# admin.site.register(Topic, TopicAdmin)
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Post)
+admin.site.unregister(Group)
+
+# Customize header and title
+admin.site.site_header = "Boards"
+admin.site.site_title = "Boards Admin Panel"
--- discussion_board/settings.py
+++ discussion_board/settings.py
@@ -81,14 +81,19 @@ WSGI_APPLICATION = "discussion_board.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Boards_django",
-        "USER": "postgres",
-        "PASSWORD": "xxxxxxxxxx",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "Boards_django",
+    #     "USER": "postgres",
+    #     "PASSWORD": "xxxxxxxxxx",
+    #     "HOST": "localhost",
+    #     "PORT": "5432",
+    # }
 }
 
 
